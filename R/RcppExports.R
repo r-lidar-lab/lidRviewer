4c9a32fc887e2ec47371768a49ef857c752db48e
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2024-08-25 09:24:25 -0400

    R cmd check

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 lidRviewer <- function(df) {
-    invisible(.Call(`_lidRviewer_lidRviewer`, df))
+    invisible(.Call('_lidRviewer_lidRviewer', PACKAGE = 'lidRviewer', df))
 }
 
